c6381fdaf3c32d6fd70adf77fdd589a60dece51d
Ivan Gutierrez <ivangutierrezraimundo@gmail.com>
2026-08-05 13:42:10 +0200

    version 41 factura 2 falta descuento y curas

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use('/admin', (_req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'img')));
 
 // Sesiones para autenticación de administrador
 app.use(
